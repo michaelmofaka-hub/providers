e6ebb86713a91720e758f350e3025c51f7f1ab97
michaelmofaka-hub <michaelmofaka@gmail.com>
2026-07-05 13:15:32 +0300

    edited nav_ bar

--- docs/app.js
+++ docs/app.js
@@ -1,8 +1,6 @@
 const menu_btn = document.getElementById("menu_toggle");
 const nav_bar = document.getElementById("nav_bar");
 
-menu_btn.addEventListener("click", ()=>{
-  if(nav_bar.style.display === 'none'){
-    nav_bar.style.display = 'flex';
-  }
+menu_btn.addEventListener("click", () => {
+  nav_bar.classList.toggle('active');
 });

05d6b9b622670694e7cbe9e697f4766935f5724d
michaelmofaka-hub <michaelmofaka@gmail.com>
2026-07-09 06:34:09 +0300

    Fix typo in nav_bar class toggle
    Signed-off-by: michaelmofaka-hub <michaelmofaka@gmail.com>

--- docs/index.js
+++ docs/index.js
@@ -2,5 +2,5 @@ const menu = document.getElementById("menu_toggle");
 const nav_bar = document.getElementById("nav_bar");
 
 menu.addEventListener("click", () => {
-    nav_ bar.classList.toggle("active");
+    nav_bar.classList.toggle("active");
 });

13bfadf723e2a5eac6f7a73b4f24fc3e983303f7
michaelmofaka-hub <michaelmofaka@gmail.com>
2026-07-11 22:16:42 +0300

    updated and modified

--- docs/index.js
+++ docs/index.js
@@ -1,12 +1,8 @@
 const menu = document.getElementById("menu_toggle");
 const nav_bar = document.getElementById("nav_bar");
+const nav_overlay = document.getElementById("nav_overlay");
 
 menu.addEventListener("click", () => {
-<<<<<<< HEAD
-    if (nav_bar.style.display === "none") {
-        nav_bar.classList.toggle("active");
-    }
-=======
     nav_bar.classList.toggle("active");
->>>>>>> 141e1063fa863a43a8f14fd7341570ee17259178
+    nav_overlay.classList.toggle("active");
 });

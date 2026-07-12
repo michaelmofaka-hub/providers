a0b479a6692e297f1f9575e6f2bd79de3c0c73cf
michaelmofaka-hub <michaelmofaka@gmail.com>
2026-07-12 16:53:43 +0300

    merged

--- Backend/index.js
+++ Backend/index.js
@@ -5,19 +5,24 @@ import dotenv from "dotenv";
 import { connectDB } from "./Config/db.js";
 import Auth from "./Routes/Auth.js";
 
+dotenv.config();
+
 const app = express();
 app.use(cors());
 app.use(express.json());
-dotenv.config();
 app.get("/health", (req, res) => {
     res.status(201).json({ message: "ok" });
     console.log("welcome world");
 });
-app.use("api/", Auth);
+app.use("/api/", Auth);
 
-const port = process.env.PORT;
+const port = process.env.PORT|8000;
 app.listen(port, () => {
+  try {
     connectDB();
     console.log(`server running on port ${port}`);
     console.log("health check in progress...");
+  } catch (error) {
+    console.log("500 error");
+  }
 });
